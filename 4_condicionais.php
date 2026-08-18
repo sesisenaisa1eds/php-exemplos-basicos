4dea235dc00e1e9455083598b159aee8ec492966
sesisenaisa1eds <131295295+sesisenaisa1eds@users.noreply.github.com>
2026-08-18 07:50:59 -0300

    Inclusão
    Mensagem de erro.

--- 4_condicionais.php
+++ 4_condicionais.php
@@ -29,5 +29,13 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
         <input type="password" name="senha" required><br>
         <button type="submit">Entrar</button>
     </form>
+
+    <?php
+        // Exibe a mensagem de erro
+        if (isset($erro)) {
+            echo "<p style='color: red';>$erro</p>";
+        }
+    ?>
+    
 </body>
 </html>
