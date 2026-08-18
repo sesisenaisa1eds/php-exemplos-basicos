a1db40529fcc498fcd1e5cdb3ec02d9af618c825
sesisenaisa1eds <131295295+sesisenaisa1eds@users.noreply.github.com>
2026-08-18 10:50:25 -0300

    Inclusão
    Assunto 2.

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro - status Codes</title>
+</head>
+
+<body>
+    <h1>Cadastro de alunos (Com status Codes)</h1>
+    
+    <form method="post" action="">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required><br><br>
+        
+        <label for="idade">Idade:</label>
+        <input type="text" name="idade" required><br><br>
+        
+        <button type="submit">Enviar</button>
+
+        
+    </form>
+    
+    
+</body>
+</html>
